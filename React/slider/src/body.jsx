22757560cd221235188d36d7755b0ee506afb52f
MehmedNdj <mehmednedjatin13@gmail.com>
2024-04-02 17:51:01 +0300

    update

--- React/slider/src/body.jsx
+++ React/slider/src/body.jsx
@@ -7,14 +7,35 @@ function RGBSlider() {
 
   const updateColor = () => `rgb(${red}, ${green}, ${blue})`;
 
+  const increaseValue = (colorSetter) => {
+    colorSetter((prevValue) => Math.min(prevValue + 5, 255));
+  };
+
+  const decreaseValue = (colorSetter) => {
+    colorSetter((prevValue) => Math.max(prevValue - 5, 0));
+  };
+
   return (
     <div>
-      <div className="box" style={{backgroundColor: updateColor(), width: '400px', height: '200px'}}>
-      </div>
+      <div className="box" style={{ backgroundColor: updateColor() }}></div>
       <div className="color-sliders">
-        <input type="range" min="0" max="256" value={red} onChange={e => setRed(e.target.value)} />
-        <input type="range" min="0" max="256" value={green} onChange={e => setGreen(e.target.value)} />
-        <input type="range" min="0" max="256" value={blue} onChange={e => setBlue(e.target.value)} />
+        <div className="slider-group">
+          <button onClick={() => increaseValue(setRed)}>+</button>
+          <input id="red" type="range" min="0" max="255" value={red} onChange={(e) => setRed(e.target.value)} />
+          <button onClick={() => decreaseValue(setRed)}>-</button>
+        </div>
+
+        <div className="slider-group">
+          <button onClick={() => increaseValue(setGreen)}>+</button>
+          <input id="green" type="range" min="0" max="255" value={green} onChange={(e) => setGreen(e.target.value)} />
+          <button onClick={() => decreaseValue(setGreen)}>-</button>
+        </div>
+
+        <div className="slider-group">
+          <button onClick={() => increaseValue(setBlue)}>+</button>
+          <input id="blue" type="range" min="0" max="255" value={blue} onChange={(e) => setBlue(e.target.value)} />
+          <button onClick={() => decreaseValue(setBlue)}>-</button>
+        </div>
       </div>
     </div>
   );
