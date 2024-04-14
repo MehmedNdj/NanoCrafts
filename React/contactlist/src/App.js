428f3ba19da00c5314e6db75e1d6ff5f47710702
MehmedNdj <mehmednedjatin13@gmail.com>
2024-04-14 16:54:34 +0300

    update

--- React/contactlist/src/App.js
+++ React/contactlist/src/App.js
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
+import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 function App() {
+
+  const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  }
+
   return (
-    <p>a</p>
+    <FormControl>
+      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={age}
+        label="Age"
+        onChange={handleChange}>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
   );
 }
 
