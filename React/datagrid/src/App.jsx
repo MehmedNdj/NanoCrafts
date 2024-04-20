9f9f98d2128956d13d4c48ba725d918ebad15329
MehmedNdj <mehmednedjatin13@gmail.com>
2024-04-20 17:18:59 +0300

    testing done

--- React/datagrid/src/App.jsx
+++ React/datagrid/src/App.jsx
@@ -1,4 +1,11 @@
 import { useState } from 'react'
+import DataGrid from './Datagrid.jsx'
+
+function App() {
+  return(
+    <DataGrid/>
+  )
+}
 
 
 export default App
---
+++ React/datagrid/src/Datagrid.jsx
@@ -0,0 +1,10 @@
+function DataGrid(){
+    return (
+        <div>
+            <h1>Datagrid</h1>
+            <p>Here is the datagrid</p>
+        </div>    
+    )
+}
+
+export default DataGrid;
